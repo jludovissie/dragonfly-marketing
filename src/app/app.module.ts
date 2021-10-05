11d47b5c2e2ff9694bc6c5a0c65a936a7ae40f7f
Jordan Ludovissie <Jludovissie24@gmail.com>
2021-10-04 20:34:02 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderNavigationComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule
